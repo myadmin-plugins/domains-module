f3455db1a6b0682745a37ccead1fb72a04fce487
Joe Huss <detain@interserver.net>
2017-06-24 13:37:35 -0400

    updates setting up more functionality in the plugin

--- src/Plugin.php
+++ src/Plugin.php
@@ -11,6 +11,23 @@ class Plugin {
 	public static $help = '';
 	public static $module = 'domains';
 	public static $type = 'module';
+	public static $settings = [
+		'SERVICE_ID_OFFSET' => 10000,
+		'USE_REPEAT_INVOICE' => true,
+		'USE_PACKAGES' => true,
+		'BILLING_DAYS_OFFSET' => 45,
+		'IMGNAME' => 'server_add_48.png',
+		'REPEAT_BILLING_METHOD' => PRORATE_BILLING,
+		'DELETE_PENDING_DAYS' => 45,
+		'SUSPEND_DAYS' => 14,
+		'SUSPEND_WARNING_DAYS' => 7,
+		'TITLE' => 'Domain Registrations',
+		'MENUNAME' => 'Domains',
+		'EMAIL_FROM' => 'support@interserver.net',
+		'TBLNAME' => 'Domains',
+		'TABLE' => 'domains',
+		'TITLE_FIELD' => 'domain_hostname',
+		'PREFIX' => 'domain'];
 
 
 	public function __construct() {
