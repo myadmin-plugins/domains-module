d4dfb42ea6e6c5ae5018151627e631273ad78f0d
Joe Huss <detain@interserver.net>
2017-06-24 07:58:36 -0400

    improvmenets to module handling

--- src/Plugin.php
+++ src/Plugin.php
@@ -18,8 +18,8 @@ class Plugin {
 
 	public static function getHooks() {
 		return [
-			'domains.load_processing' => [__CLASS__, 'loadProcessing'],
-			'domains.settings' => [__CLASS__, 'getSettings'],
+			self::$module.'.load_processing' => [__CLASS__, 'loadProcessing'],
+			self::$module.'.settings' => [__CLASS__, 'getSettings'],
 		];
 	}
 
@@ -29,6 +29,6 @@ class Plugin {
 
 	public static function getSettings(GenericEvent $event) {
 		$settings = $event->getSubject();
-		$settings->add_dropdown_setting('domains', 'General', 'outofstock_domains', 'Out Of Stock Domains', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_DOMAINS'), array('0', '1'), array('No', 'Yes', ));
+		$settings->add_dropdown_setting(self::$module, 'General', 'outofstock_domains', 'Out Of Stock Domains', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_DOMAINS'), array('0', '1'), array('No', 'Yes', ));
 	}
 }
