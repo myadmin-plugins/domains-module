46aa35ecc3514b70da763c320ef9f9813d82e21c
Joe Huss <detain@interserver.net>
2019-11-16 10:48:24 -0500

    a bunch of updates switching out old admin_mail code with the new Mail class adminMail call and optmizing things here and there as i go'

--- src/Plugin.php
+++ src/Plugin.php
@@ -75,11 +75,7 @@ class Plugin
 				$smarty->assign('domain_name', $serviceTypes[$serviceInfo[$settings['PREFIX'].'_type']]['services_name']);
 				$email = $smarty->fetch('email/admin/domain_created.tpl');
 				$subject = 'New Domain Created '.$serviceInfo[$settings['TITLE_FIELD']];
-				$headers = '';
-				$headers .= 'MIME-Version: 1.0'.PHP_EOL;
-				$headers .= 'Content-type: text/html; charset=UTF-8'.PHP_EOL;
-				$headers .= 'From: '.TITLE.' <'.EMAIL_FROM.'>'.PHP_EOL;
-				admin_mail($subject, $email, $headers, false, 'admin/domain_created.tpl');
+				(new MyAdmin\Mail())->adminMail($subject, $email, false, 'admin/domain_created.tpl');
 			})->setReactivate(function ($service) {
 				$serviceTypes = run_event('get_service_types', false, self::$module);
 				$serviceInfo = $service->getServiceInfo();
@@ -92,11 +88,7 @@ class Plugin
 				$smarty->assign('domain_name', $serviceTypes[$serviceInfo[$settings['PREFIX'].'_type']]['services_name']);
 				$email = $smarty->fetch('email/admin/domain_reactivated.tpl');
 				$subject = $serviceInfo[$settings['TITLE_FIELD']].' '.$serviceTypes[$serviceInfo[$settings['PREFIX'].'_type']]['services_name'].' '.$settings['TBLNAME'].' Reactivated';
-				$headers = '';
-				$headers .= 'MIME-Version: 1.0'.PHP_EOL;
-				$headers .= 'Content-type: text/html; charset=UTF-8'.PHP_EOL;
-				$headers .= 'From: '.TITLE.' <'.EMAIL_FROM.'>'.PHP_EOL;
-				admin_mail($subject, $email, $headers, false, 'admin/domain_reactivated.tpl');
+				(new MyAdmin\Mail())->adminMail($subject, $email, false, 'admin/domain_reactivated.tpl');
 			})->setDisable(function () {
 			})->register();
 	}
